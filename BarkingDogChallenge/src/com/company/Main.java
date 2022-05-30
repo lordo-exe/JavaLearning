fd2a22a973865d2898bb22b6eca710c190a9300f
lordo-exe <kerilivanchuk@gmail.com>
2022-05-30 17:28:09 -0500

    Challenge done.

--- BarkingDogChallenge/src/com/company/Main.java
+++ BarkingDogChallenge/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+
+    public static boolean shouldWakeUp (boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) { // Making sure the hourOfDay value is within parameter
+            return false;
+        } else if (!barking) { // If dog is not barking, instantly return false
+            return false;
+        } else return hourOfDay < 8 || hourOfDay > 22;
+    } // END of shouldWakeUp method
 }
