9f99166a0081d2d7e19646d0ee7d9b3aec8400dc
lordo-exe <kerilivanchuk@gmail.com>
2022-06-03 11:31:21 -0500

    Testing and adjustments made.

--- DecimalComparatorChallenge/src/com/company/Main.java
+++ DecimalComparatorChallenge/src/com/company/Main.java
@@ -3,13 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("This is main result = " + areEqualByThreeDecimalPlaces(155.155, 155.155));
+    System.out.println("This is main result = " + areEqualByThreeDecimalPlaces(155.15122, 155.15522));
     }
 
     public static boolean areEqualByThreeDecimalPlaces (double x, double y) {
+//        double test1 = y * 1000;
+//        int test2 = (int) (x * 1000);
+//        System.out.println("This is x = " + test2 + " and this is y = " + test1);
 
-        if (x * 1000 == y * 1000) {
-      System.out.println("This is x = " + x + " and this is y = " + y);
+        if ((int) x * 1000 == (int) y * 1000 && x == y) {
             return true;
         }
         return false;
