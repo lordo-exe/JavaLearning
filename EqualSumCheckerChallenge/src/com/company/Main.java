0e5748f75cb77c090897913abb980ae1eedc5660
lordo-exe <kerilivanchuk@gmail.com>
2022-06-04 19:03:36 -0500

    Created and completed challenge.

---
+++ EqualSumCheckerChallenge/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static boolean hasEqualSum (int x, int y, int z) {
+        return x + y == z; // If x + y = z return true, otherwise return false
+    } // END hasEqualSum method
+}
