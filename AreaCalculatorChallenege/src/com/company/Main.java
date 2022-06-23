2e3c5b01977d8928f7e73f2a96f81fb8f23c9dce
lordo-exe <kerilivanchuk@gmail.com>
2022-06-23 18:32:35 -0500

    AreaCalculatorChallenge
    Challenge started.

---
+++ AreaCalculatorChallenege/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static double area (double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+
+    }
+}
