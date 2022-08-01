a2df7d45d0302c6f860a596f474da3d05896b23d
lordo-exe <kerilivanchuk@gmail.com>
2022-07-31 21:41:28 -0500

    Completed / Added Comments

--- MinutesToYearsToDaysChallenge/src/com/company/Main.java
+++ MinutesToYearsToDaysChallenge/src/com/company/Main.java
@@ -8,13 +8,13 @@ public class Main {
     }
 
     public static void printYearsAndDays (long minutes) {
-        if (minutes < 0) {
-        System.out.println(INVALID_VALUE_MESSAGE);
+        if (minutes < 0) { // Checking to make sure the value fits valid parameters
+        System.out.println(INVALID_VALUE_MESSAGE); // Constant for printing "Invalid Value"
         } else {
-            long daysCalc = minutes / 1440;
-            long days = daysCalc % 365;
-            long years = daysCalc / 365;
-            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+            long daysCalc = minutes / 1440; // Calculating minutes into days
+            long days = daysCalc % 365; // Display value for days, only displays the remainder of days no more than 365
+            long years = daysCalc / 365; // Years value
+            System.out.println(minutes + " min = " + years + " y and " + days + " d"); // Message
         }
     }
 }
